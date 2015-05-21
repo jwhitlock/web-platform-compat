816d2ba506b14dd3eaacee9f1269779749b9e254
John Whitlock <John-Whitlock@ieee.org>
2015-05-21 11:33:14 -0500

    bug 1132658 - Add doc_parse_error issue
    Some pages don't have the expected structure, such as <h2> headings,
    resulting in the doc rule not matching.  Turn this into a
    doc_parse_error issue, instead of an Exception, for further processing.
    
    Example:
    
    https://developer.mozilla.org/en-US/docs/Navigation_timing

--- mdn/models.py
+++ mdn/models.py
@@ -275,6 +275,11 @@ ISSUES = {
         'Unknown Gecko version "{version}"',
         'The importer does not recognize this version for CompatGeckoFxOS.'
         ' Change the MDN page or update the importer.'),
+    'doc_parse_error': (
+        CRITICAL,
+        'No imported data due to unexpected page structure.',
+        'The importer was unable to handle the page structure enough to'
+        ' determine if there was compatibility data.'),
     'exception': (CRITICAL, 'Unhandled exception', '{traceback}'),
     'extra_cell': (
         ERROR,
