446d5bf685cb27e771a9a88e9e06066c0488b7b6
Anthony Maton <maitre.negi@gmail.com>
2016-04-08 21:05:56 +0200

    bug 1251252 - Part 1 : Add a migration to allow name to be empty

---
+++ webplatformcompat/migrations/0022_sections_name_allow_blank.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import webplatformcompat.validators
+import webplatformcompat.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('webplatformcompat', '0021_drop_feature_section_m2m'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='section',
+            name='name',
+            field=webplatformcompat.fields
+            .TranslatedField(help_text='Name of section, without section number'
+                             , validators=[webplatformcompat.validators
+                                           .LanguageDictValidator(False)],
+                             blank=True),
+        ),
+    ]
+
